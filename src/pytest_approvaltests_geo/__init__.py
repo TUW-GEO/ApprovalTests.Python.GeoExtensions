dc8b5de1dbee396c645386c72aed3b9b7d670e5b
Bernhard Raml <bernhard.raml@geo.tuwien.ac.at>
2022-10-14 08:30:49 +0200

    F!! add geo data configuration

--- src/pytest_approvaltests_geo/__init__.py
+++ src/pytest_approvaltests_geo/__init__.py
@@ -1,20 +1,38 @@
+from pathlib import Path
+
 from pytest_approvaltests_geo._version import __version__
 import pytest
+
+APPROVAL_TEST_GEO_DATA_ROOT_OPTION = "--approval-test-geo-data-root"
 
 
 def pytest_addoption(parser):
     group = parser.getgroup('pytest-approvaltests-geo')
-    group.addoption(
-        '--foo',
-        action='store',
-        dest='dest_foo',
-        default='{% now "utc", "%Y" %}',
-        help='Set the value for the fixture "bar".'
-    )
+    group.addoption(APPROVAL_TEST_GEO_DATA_ROOT_OPTION,
+                    default=None,
+                    help="specify local approval test data root")
 
-    parser.addini('HELLO', 'Dummy pytest.ini setting')
+    parser.addini('approvaltests_geo_data_root',
+                  'Path to your approval test geo data root containing your input and approved files', type='string')
+    parser.addini('approvaltests_geo_input',
+                  'Subdirectory within the approval test geo data root containing your input files', type='string')
+    parser.addini('approvaltests_geo_approved',
+                  'Subdirectory within the approval test geo data root containing your approved files', type='string')
 
 
 @pytest.fixture
-def bar(request):
-    return request.config.option.dest_foo
+def approval_test_geo_data_root(request):
+    custom_root = request.config.option.approval_test_geo_data_root
+    if custom_root is not None:
+        return Path(custom_root)
+    return Path(request.config.getini('approvaltests_geo_data_root'))
+
+
+@pytest.fixture
+def approval_geo_input_directory(approval_test_geo_data_root, request):
+    return approval_test_geo_data_root / request.config.getini('approvaltests_geo_input')
+
+
+@pytest.fixture
+def approved_geo_directory(approval_test_geo_data_root, request):
+    return approval_test_geo_data_root / request.config.getini('approvaltests_geo_approved')
--- tests/test_approvaltests_geo.py
+++ tests/test_approvaltests_geo.py
@@ -1,59 +1,48 @@
-def test_bar_fixture(testdir):
-    """Make sure that pytest accepts our fixture."""
-
-    # create a temporary pytest test module
-    testdir.makepyfile("""
-        def test_sth(bar):
-            assert bar == "europython2015"
-    """)
-
-    # run pytest with the following cmd args
-    result = testdir.runpytest(
-        '--foo=europython2015',
-        '-v'
-    )
-
-    # fnmatch_lines does an assertion internally
-    result.stdout.fnmatch_lines([
-        '*::test_sth PASSED*',
-    ])
-
-    # make sure that that we get a '0' exit code for the testsuite
-    assert result.ret == 0
-
-
-def test_help_message(testdir):
-    result = testdir.runpytest(
-        '--help',
-    )
-    # fnmatch_lines does an assertion internally
-    result.stdout.fnmatch_lines([
-        'pytest-approvaltests-geo:',
-        '*--foo=DEST_FOO*Set the value for the fixture "bar".',
-    ])
-
-
-def test_hello_ini_setting(testdir):
+def test_approvaltests_geo_data_settings(testdir):
     testdir.makeini("""
         [pytest]
-        HELLO = world
+        approvaltests_geo_data_root = /path/to/my/approved/geo/data/root/
+        approvaltests_geo_input = subdir/to/input/data/
+        approvaltests_geo_approved = subdir/to/approved/data/
     """)
 
     testdir.makepyfile("""
         import pytest
-        @pytest.fixture
-        def hello(request):
-            return request.config.getini('HELLO')
-        def test_hello_world(hello):
-            assert hello == 'world'
+        def test_approved_geo_paths(approval_test_geo_data_root, approval_geo_input_directory, approved_geo_directory):
+            assert approval_test_geo_data_root.as_posix() == '/path/to/my/approved/geo/data/root'
+            assert approval_geo_input_directory.as_posix() == '/path/to/my/approved/geo/data/root/subdir/to/input/data'
+            assert approved_geo_directory.as_posix() == '/path/to/my/approved/geo/data/root/subdir/to/approved/data'
     """)
 
     result = testdir.runpytest('-v')
 
-    # fnmatch_lines does an assertion internally
     result.stdout.fnmatch_lines([
-        '*::test_hello_world PASSED*',
+        '*::test_approved_geo_paths PASSED*',
     ])
 
-    # make sure that that we get a '0' exit code for the testsuite
+    assert result.ret == 0
+
+
+def test_approval_test_geo_data_root_option(testdir):
+    testdir.makeini("""
+        [pytest]
+        approvaltests_geo_data_root = /default/path/to/geo/test/data/root/
+        approvaltests_geo_input = subdir/to/input/data/
+        approvaltests_geo_approved = subdir/to/approved/data/
+    """)
+
+    testdir.makepyfile("""
+        def test_custom_approval_test_geo_data_root(approval_test_geo_data_root):
+            assert approval_test_geo_data_root.as_posix() == "/custom/geo/data/root"
+    """)
+
+    result = testdir.runpytest(
+        '--approval-test-geo-data-root=/custom/geo/data/root',
+        '-v'
+    )
+
+    result.stdout.fnmatch_lines([
+        '*::test_custom_approval_test_geo_data_root PASSED*',
+    ])
+
     assert result.ret == 0
