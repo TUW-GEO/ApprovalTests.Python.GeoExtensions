22d6f22a0a748cab64607d532e3acec6367c0975
Bernhard Raml <bernhard.raml@geo.tuwien.ac.at>
2022-10-14 07:57:03 +0200

    t add test checking meta data reporting

--- tests/test_report_geo_tiffs.py
+++ tests/test_report_geo_tiffs.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+
 import pytest
+from approval_utilities.utils import to_json
+from approvaltests.scrubbers import scrub_all_dates
 
 from factories import make_raster_at
 from pytest_approvaltests_geo.report_geo_tiffs import ReportGeoTiffs
@@ -20,3 +24,11 @@ def test_report_pixel_difference_statistics(reporter, tmp_path, capsys):
     approved = make_raster_at([[2, 4], [-2, 1]], tmp_path / "approved.tif", dict(some='tag'))
     assert reporter.report(received.as_posix(), approved.as_posix())
     assert "min=1, max=4, mean=2.25, median=2" in capsys.readouterr().out
+
+
+def test_report_meta_data_differences(reporter, tmp_path, capsys):
+    received = make_raster_at([[42]], tmp_path / "received.tif", dict(some='tag'))
+    approved = make_raster_at([[42]], tmp_path / "approved.tif", dict(some='other'))
+    assert reporter.report(received.as_posix(), approved.as_posix())
+    output = capsys.readouterr().out
+    assert '+    "some": "tag"' in output and '-    "some": "other"' in output
